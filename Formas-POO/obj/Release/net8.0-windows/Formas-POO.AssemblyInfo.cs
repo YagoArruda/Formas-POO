9293d532119a33e4ede1870d797b6f7b3d9e41cb
YagoArruda <yago4444@gmail.com>
2024-04-12 22:10:54 -0300

    Arquivo de classes

--- Formas-POO/obj/Release/net8.0-windows/Formas-POO.AssemblyInfo.cs
+++ Formas-POO/obj/Release/net8.0-windows/Formas-POO.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Formas-POO")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b3e72718288fc116c86a60db8d867b270147a4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Formas-POO")]
